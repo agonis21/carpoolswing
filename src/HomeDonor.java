70a96917dc9a0901ad3c3af896bb98d67c2a4ea2
Steven Rogers <xxsteven.rogersxx@gmail.com>
2023-02-16 22:00:58 -0500

    Steven Homepage updates and division

---
+++ src/HomeDonor.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class HomeDonor {
+    private JPanel Header;
+    private JToolBar Navbar;
+    private JButton HomeButton;
+    private JButton TutorialButton;
+    private JButton AboutButton;
+    private JButton AccountButton;
+    private JLabel logoLabel;
+    private JButton DonateButton;
+    private JLabel HaveJobLabel;
+    private JPanel BackgroundPanel;
+}
