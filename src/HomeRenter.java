15fdbde7c110e4ee0a43d7615db4206e99395a2d
Steven Rogers <xxsteven.rogersxx@gmail.com>
2023-02-17 20:35:48 -0500

    Added All Steven Pages and Variants

---
+++ src/HomeRenter.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class HomeRenter {
+    private JButton RentButton;
+    private JPanel BackgroundPanel;
+    private JPanel Header;
+    private JLabel logoLabel;
+    private JButton HomeButton;
+    private JButton TutorialButton;
+    private JButton AboutButton;
+    private JButton AccountButton;
+    private JLabel HaveJobLabel;
+    private JToolBar Navbar;
+
+}
---
+++ src/TutorialPageRenter.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class TutorialPageRenter {
+    private JPanel Header;
+    private JToolBar Navbar;
+    private JButton HomeButton;
+    private JButton TutorialButton;
+    private JButton AboutButton;
+    private JButton AccountButton;
+    private JLabel logoLabel;
+    private JLabel RenterTutorialLabel;
+    private JLabel Step1Label;
+    private JLabel Step2Label;
+    private JLabel Step3Label;
+    private JLabel SideNoteLabel;
+    private JLabel Step2ParenthLabel;
+}
