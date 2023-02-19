2be793d5531b4b5e5c03c22a119c86b5ec831067
asmgoni <asm.goni21@my.stjohns.edu>
2023-02-19 00:26:55 -0500

    admin details + tabbed pane for jobs, users, vehicles

---
+++ src/AdminPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AdminPanel {
+    private JLabel adminrID_label;
+    private JLabel adminName_label;
+    private JLabel currentDate_label;
+    private JLabel currentTime_label;
+    private JPanel adminDetails_panel;
+    private JPanel adminContent_panel;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane3;
+}
