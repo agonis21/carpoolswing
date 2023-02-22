c0ed8d74b390316559ed8eae667b13e730260648
FairoozBintye <fairooz.bintye20@my.stjohns.edu>
2023-02-21 23:52:18 -0500

    new commit

--- src/JobDetails.java
+++ src/JobDetails.java
@@ -51,7 +51,7 @@ public class JobDetails extends JFrame {
 
     }
 
-//    public static void main(String[] args) {
+ //  public static void main(String[] args) {
 //        JobDetails jobDet = new JobDetails();
 //
 //    }
